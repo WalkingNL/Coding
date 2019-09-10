1a635c4445600475bfdcce36444b7437c79b19b9
NL.Gong <932382981@qq.com>
2019-09-10 10:16:24 +0800

    Update BinaryTree.h

--- BinaryTree.h
+++ BinaryTree.h
@@ -1,3 +1,9 @@
+/**************************************************************/
+// gongqb June 6, 2019.
+// How to construct a BinaryTree using Chain structure
+// In the code blow, I refer to a book on how to name 
+// variables and functions
+/**************************************************************/
 #pragma once
 
 struct BinaryTreeNode
@@ -7,12 +13,15 @@ struct BinaryTreeNode
 	BinaryTreeNode* m_pRight; // Right Child
 };
 
+// construct a Node of tree.
 BinaryTreeNode* CreateBinaryTreeNode(int value);
 
+// to build BinaryTree by connecting Nodes
 void ConnectTreeNoes(BinaryTreeNode* pParent, BinaryTreeNode* pLeft, BinaryTreeNode* pRight);
 
+// to output a BinaryTree
 void PrintTreeNode(const BinaryTreeNode* pNode);
-
 void PrintTree(const BinaryTreeNode* pRoot);
 
+// to destroy a BinaryTree
 void DestroyTree(BinaryTreeNode* pRoot);
